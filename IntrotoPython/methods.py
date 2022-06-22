5dcfc48fbd4446602cf8f783c26f17ab2b9d7184
AdaSno5 <106356053+AdaSno5@users.noreply.github.com>
2022-06-22 16:56:04 -0400

    added append and reverse exercises

--- IntrotoPython/methods.py
+++ IntrotoPython/methods.py
@@ -20,3 +20,22 @@ print(areas.index(20.0))
 
 # Print out how often 9.50 appears in areas
 print(areas.count(9.50))
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Use append twice to add poolhouse and garage size
+areas.append(24.5)
+print(areas)
+areas.append(15.45)
+print(areas)
+
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas
+areas.reverse()
+
+# Print out areas
+print(areas)
