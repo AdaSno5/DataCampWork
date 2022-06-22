eb3958ebb89f828dddf308bf54443309a1dd8a71
AdaSno5 <106356053+AdaSno5@users.noreply.github.com>
2022-06-22 15:51:29 -0400

    created new file

--- IntrotoPython/Multiple_arguments.py
+++ IntrotoPython/Multiple_arguments.py
@@ -14,3 +14,5 @@ full_sorted=sorted(full, reverse=True)
 
 # Print out full_sorted
 print(full_sorted)
+
+
---
+++ IntrotoPython/methods.py
@@ -0,0 +1,13 @@
+# string to experiment with: place
+place = "poolhouse"
+str.upper(place)
+place_up='POOLHOUSE'
+# Use upper() on place: place_up
+str.upper(place_up)
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+print(place.count("o"))
